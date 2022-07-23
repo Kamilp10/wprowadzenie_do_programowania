2b7aa895925e3995d437abf56e50f814a890bd29
Kamil <kamil.pietrzak4235@gmail.com>
2022-07-23 14:50:30 +0200

    stworzenie nowej klasy, która pyta uzytkownika o imie,nazwisko i wiek oraz sprawdza czy uzytkownik moze wejsc do klubu (warunek 18 lat). Jezeli imie uzytkownika znajduje sie na liscie wygranych imion, dostaje darmowego szota przy wejsciu :)

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -2,9 +2,12 @@ public class ForLoop {
     public static void main(String[] args) {
 
         String[] kitties = new String[]{"psotek", "łatek", "bąbel", "baryła", "simba"};
-        for (int i = 0;  i<kitties.length; i++){
+        for (int i = 0;  i<kitties.length; i++) {
             System.out.println(kitties[i]);
-
+        }
+            for (String kitty: kitties){
+                System.out.println(kitty);
             }
+
         }
     }
---
+++ src/NoweZadanko.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class NoweZadanko {
+    public static void main(String[] args) {
+        /**
+         * 1.chce aby w programie było pytanie o imie i nazwisko i wiek
+         * 2.chce zeby po zapytaniu o wiek byla petla sprawdzajaca czy osoba jest pelnoletnia czy nie - (przy pelnoletniej napis zapraszam do klubu, a jezeli nie to napis wypad)
+         *
+         */
+        String[] wygrani = new String[]{"Ania", "Zosia", "Magda", "Piotrek", "Adam"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj imie");
+        String imie = scanner.next();
+        System.out.println("podaj nazwisko");
+        String naziwsko = scanner.next();
+        System.out.println("Podaj wiek");
+        int wiek = scanner.nextInt();
+        if (wiek>=18){
+            System.out.println("zapraszam do klubu");
+            for (String wygrany: wygrani){
+                if(Objects.equals(imie, wygrany)){
+                    System.out.println("Dostajesz darmowego szota ;)");
+                }
+            }
+        }
+        if(wiek<18){
+            System.out.println("wypad xD");
+        }
+
+    }
+}
