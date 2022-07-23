13c96b8472eec5f977620be694c9b4d8648d3896
Kamil <kamil.pietrzak4235@gmail.com>
2022-07-23 13:24:51 +0200

    dodanie pętli for, która wyświetla zawartosć tabeli

---
+++ src/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        String[] kitties = new String[]{"psotek", "łatek", "bąbel", "baryła", "simba"};
+        for (int i = 0;  i<kitties.length; i++){
+            System.out.println(kitties[i]);
+
+            }
+        }
+    }
